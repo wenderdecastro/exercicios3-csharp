6c3def6853154be77868cc340af8715cb18eb2b0
Wender <wendermaiac@gmail.com>
2023-04-14 08:54:24 -0300

    exercicios finalizados

--- exercicio2/Program.cs
+++ exercicio2/Program.cs
@@ -1,54 +1,95 @@
-﻿// 2 - Um posto está vendendo combustíveis com a seguinte tabela de descontos:
-// Álcool:
-// . até 20 litros, desconto de 3% por litro Álcool
-// . acima de 20 litros, desconto de 5% por litro
-// Gasolina:
-// . até 20 litros, desconto de 4% por litro Gasolina
-// . acima de 20 litros, desconto de 6% por litro
-
-// Escreva um algoritmo que leia o número de litros vendidos e o tipo de combustível (codificado
-// da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente
-// sabendo-se que o preço do litro da gasolina é R$ 5,30 e o preço do litro do álcool é R$ 4,90.
-// Dica: utilize switch case e funções/métodos para otimizar o algorítimo.
-
-Console.WriteLine("Quantos litros de Alcool foram vendidos? ");
-float litrosAlcool = float.Parse(Console.ReadLine());
-
-Console.WriteLine("Quantos litros de Gasolina foram vendidos? ");
-float litrosGasolina = float.Parse(Console.ReadLine());
-
-Console.WriteLine(CalcularValores(litrosAlcool, litrosGasolina));
-
-static (float, float) CalcularValores(float a, float g)
+﻿ConsoleKeyInfo combustivelInput;
+double preco = 0;
+double litros;
+string combustivel = "";
+double total;
+bool doisCombustiveis = true;
+do
 {
-    float precoGasolina = 5.30f;
-    float precoAlcool = 4.90f;
-    float totalGasolina;
-    float totalAlcool;
-
-    if (a <= 20)
+    do
     {
-        precoAlcool -= precoAlcool * 0.03f;
-        totalAlcool = Math.Round(precoAlcool * a,2);
-    }
-    else
+        Console.WriteLine(@$"
+    Escolha o combustível desejado: 
+    1- Gasolina
+    2- Alcool
+    ");
+        combustivelInput = Console.ReadKey();
+
+        if (combustivelInput.Key == ConsoleKey.D1)
+        {
+            preco = 5.30f;
+            combustivel = "Gasolina";
+        }
+        else if (combustivelInput.Key == ConsoleKey.D2)
+        {
+            preco = 4.90f;
+            combustivel = "Alcool";
+        }
+        else
+        {
+            Console.WriteLine($"\n Opção inválida! Tente novamente. ");
+        }
+
+    } while (combustivelInput.Key != ConsoleKey.D1 && combustivelInput.Key != ConsoleKey.D2);
+
+    do
     {
-        precoAlcool -= precoAlcool * 0.05f;
-        totalAlcool = Math.Round(precoAlcool * a,2);
+
+        Console.WriteLine($"\n Digite a quantidade de litros: ");
+
+        litros = double.Parse(Console.ReadLine());
+
+    }
+    while (litros.GetType() != typeof(double));
+
+    double CalcularTotal(double l, double p)
+    {
+
+        switch (combustivel)
+        {
+            case "Gasolina" when litros < 20:
+                p -= p * 0.03f;
+                total = Math.Round(p * l, 2);
+                return (total);
+            case "Gasolina" when litros >= 20:
+                p -= p * 0.05f;
+                total = Math.Round(p * l, 2);
+                return (total);
+            case "Alcool" when litros < 20:
+                p -= p * 0.02f;
+                total = Math.Round(p * l, 2);
+                return (total);
+            case "Alcool" when litros >= 20:
+                p -= p * 0.04f;
+                total = Math.Round(p * l, 2);
+                return (total);
+            default:
+                return 0;
+
+        }
+    }
+
+    Console.WriteLine($"\n Total: {combustivel} - {CalcularTotal(litros, preco)} \n");
+
+    Console.WriteLine(@$"
+    Deseja calcular o valor de outro combustível? 
+    Pressione:
+    S para sim
+    N para não");
+
+    ConsoleKeyInfo inputDois = Console.ReadKey();
+
+    switch (inputDois.Key)
+    {
+        case ConsoleKey.S:
+            doisCombustiveis = true;
+            break;
+        case ConsoleKey.N:
+            doisCombustiveis = false;
+            break;
     }
 
 
-    if (g <= 20)
-    {
-        precoGasolina -= precoGasolina * 0.04f;
-        totalGasolina = Math.Round(precoGasolina * g,2);
-    }
-    else
-    {
-        precoGasolina -= precoGasolina * 0.06f;
-        totalGasolina = Math.Round(precoGasolina * g,2);
-    }
 
-    return (totalAlcool, totalGasolina);
-}
+} while (doisCombustiveis == true);
 
